f4235d6e5e0699204c46f7dd4c1d861eb5b80142
Timothy McClatchey <timothy@kansatech.com>
2026-05-17 20:06:27 -0500

    0.3 alpha

--- src/DocSurface.php
+++ src/DocSurface.php
@@ -5,8 +5,19 @@ declare(strict_types=1);
 namespace CommonPHP\Docs;
 
 use CommonPHP\HTTP\Contracts\HttpSurfaceInterface;
+use CommonPHP\HTTP\Request;
+use CommonPHP\HTTP\Response;
+use CommonPHP\HTTP\ResponseFactory;
 
 class DocSurface implements HttpSurfaceInterface
 {
+    public function supports(Request $request): bool
+    {
+        return $request->path() === '/docs' || str_starts_with($request->path(), '/docs/');
+    }
 
+    public function handle(Request $request): Response
+    {
+        return (new ResponseFactory())->html('<h1>Documentation page not found</h1>', 404);
+    }
 }
